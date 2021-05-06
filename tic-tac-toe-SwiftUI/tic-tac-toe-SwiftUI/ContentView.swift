b43eea317ac17ab43abcee7186f2153953fe6742
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-05-06 20:37:04 +0200

    game-creation

---
+++ tic-tac-toe-SwiftUI/tic-tac-toe-SwiftUI/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  tic-tac-toe-SwiftUI
+//
+//  Created by Frederico Kuckelhaus on 06.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+
+    var body: some View {
+        GeometryReader {
+            geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack{
+                            Circle().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).frame(width: geometry.size.width/3 - 15, height: geometry.size.height/3 - 15)
+                            Image(systemName: moves[i]?.indicator ?? "").resizable().frame(width: 40, height: 40).foregroundColor(.white)
+                        }.onTapGesture {
+                            if isOccupied(in: moves, forIndex: i){return}
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+
+        }
+
+    }
+
+    func isOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: {$0?.boardIndex == index})
+    }
+
+    enum Player {
+        case human, computer
+    }
+
+    struct Move {
+        let player: Player
+        let boardIndex: Int
+        var indicator: String {
+            return player == .human ? "xmark" : "circle"
+        }
+    }
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ tic-tac-toe-SwiftUI/tic-tac-toe-SwiftUI/tic_tac_toe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toe_SwiftUIApp.swift
+//  tic-tac-toe-SwiftUI
+//
+//  Created by Frederico Kuckelhaus on 06.05.21.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

80661c8525d71b4cd559844a1ae5e9c07758b947
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-05-10 12:08:24 +0200

    easy-AI mode

--- tic-tac-toe-SwiftUI/tic-tac-toe-SwiftUI/ContentView.swift
+++ tic-tac-toe-SwiftUI/tic-tac-toe-SwiftUI/ContentView.swift
@@ -12,7 +12,8 @@ struct ContentView: View {
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
 
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
-    @State private var isHumanTurn = true
+    @State private var isDisabled: Bool = false
+
 
     var body: some View {
         GeometryReader {
@@ -22,17 +23,27 @@ struct ContentView: View {
                 LazyVGrid(columns: columns, spacing: 5) {
                     ForEach(0..<9) { i in
                         ZStack{
-                            Circle().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).frame(width: geometry.size.width/3 - 15, height: geometry.size.height/3 - 15)
+                            Circle().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
                             Image(systemName: moves[i]?.indicator ?? "").resizable().frame(width: 40, height: 40).foregroundColor(.white)
                         }.onTapGesture {
+                            isDisabled = true
                             if isOccupied(in: moves, forIndex: i){return}
-                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
-                            isHumanTurn.toggle()
+                            moves[i] = Move(player: .human, boardIndex: i)
+
+//                            TODO: check win or draw
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                isDisabled = false
+                            }
+
+
                         }
                     }
                 }
                 Spacer()
             }
+            .disabled(isDisabled)
             .padding()
 
         }
@@ -41,6 +52,14 @@ struct ContentView: View {
 
     func isOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
         return moves.contains(where: {$0?.boardIndex == index})
+    }
+
+    func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        var movePosition = Int.random(in: 0..<9)
+        while isOccupied(in: moves, forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        return movePosition
     }
 
     enum Player {
